2332b4bcbf181c3bdebe00f3f87932f25a4a2256
smartdev1206 <tree1206@outlook.com>
2021-05-06 20:20:01 +0700

    update image api

--- accommodation/url.py
+++ accommodation/url.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 
+from accommodation.views.property_view import update_images
+
 urlpatterns = [
     path('property/', include('accommodation.urls.property')),
     path('category/', include('accommodation.urls.category')),
     path('amenity/', include('accommodation.urls.amenity')),
     path('pricing/', include('accommodation.urls.pricing')),
     path('booking/', include('accommodation.urls.booking')),
+    path('update/', update_images),
 ]
