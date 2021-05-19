0f1e19dfb9b0c468add0f62f030b71255733eec5
smartdev1206 <tree1206@outlook.com>
2021-05-19 21:26:04 +0700

    update github workflow, deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo git pull origin master
 sudo pip3 install -r requirements.txt
-sudo systemctl restart gunicorn
 sudo systemctl restart gunicorn.socket
+sudo systemctl restart gunicorn
 sudo systemctl restart nginx
