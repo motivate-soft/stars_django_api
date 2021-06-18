e58385eb44fba6b8030d91fe872c290382242b1f
smartdev1206 <tree1206@outlook.com>
2021-06-18 17:45:18 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 sudo git pull origin master
 source venv/bin/activate
 pip3 install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
 sudo systemctl restart gunicorn.socket
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
