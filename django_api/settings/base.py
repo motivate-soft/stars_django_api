29e073418613699c333a12202d1473217b749c81
smartdev1206 <tree1206@outlook.com>
2021-08-09 08:33:51 +0700

    update setting, gitignore

--- django_api/settings/base.py
+++ django_api/settings/base.py
@@ -172,3 +172,32 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 BOOKERVILLE_API_KEY = env('BOOKERVILLE_API_KEY')
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+#             'style': '{',
+#         },
+#         'simple': {
+#             'format': '{levelname} {message}',
+#             'style': '{',
+#         },
+#     },
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'log/debug.log',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
